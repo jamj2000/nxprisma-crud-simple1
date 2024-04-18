cfa3a38b4d7deece7c4e5972bce308a65b2d9b3e
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-04-18 18:40:21 +0200

    force-dynamic

--- src/app/articulos/page.js
+++ src/app/articulos/page.js
@@ -6,7 +6,7 @@ import {
 } from '@/lib/actions'
 
 
-// export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic'
 
 export default async function Home() {
     const articulos = await getArticulos()
