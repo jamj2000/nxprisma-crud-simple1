a898cb949091e502cb8d31ce40259d92a961d712
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-04-18 17:48:26 +0200

    ...

---
+++ src/app/articulos/page.js
@@ -0,0 +1,62 @@
+import {
+    getArticulos,
+    newArticulo,
+    updateArticulo,
+    deleteArticulo
+} from '@/lib/actions'
+
+
+// export const dynamic = 'force-dynamic'
+
+export default async function Home() {
+    const articulos = await getArticulos()
+    // console.log(articulos);
+
+    return (
+        <>
+            <label>Buscar:
+                <input type='search' defaultValue={search} placeholder='nombre de artículo' />
+            </label>
+            <form>
+                <label>Nombre:
+                    <input name='nombre' />
+                </label>
+
+                <label>Descripción:
+                    <input name='descripcion' />
+                </label>
+
+                <label>Precio en €:
+                    <input name='precio' type='number' step='0.01' />
+                </label>
+
+                <button formAction={newArticulo}>Nuevo artículo</button>
+            </form>
+            {
+                articulos
+                    .sort((a, b) => b.id - a.id)  // Orden inverso, de id mayor a menor
+                    .map((articulo) => (
+                        <form key={articulo.id}>
+                            <input name='id' value={articulo.id} type='hidden' />
+
+                            <label>Nombre:
+                                <input name='nombre' defaultValue={articulo.nombre} />
+                            </label>
+
+                            <label>Descripción:
+                                <input name='descripcion' defaultValue={articulo.descripcion} />
+                            </label>
+
+                            <label>Precio en €:
+                                <input name='precio' defaultValue={(articulo.precio / 100).toString()} type='number' step='0.01' />
+                            </label>
+
+                            <button formAction={updateArticulo}>Actualizar artículo</button>
+                            <button formAction={deleteArticulo}>Eliminar artículo</button>
+                        </form>
+
+                    ))
+            }
+        </>
+    )
+}
---
+++ src/lib/actions.js
@@ -0,0 +1,68 @@
+'use server'
+import prisma from '@/lib/prisma'
+import { revalidatePath } from 'next/cache';
+
+
+export async function getArticulos() {
+  try {
+    const articulos = await prisma.articulo.findMany()
+    return articulos;
+  } catch (error) {
+    return null;
+  }
+}
+
+
+export async function newArticulo(formData) {
+  try {
+    const nombre = formData.get('nombre')
+    const descripcion = formData.get('descripcion')
+    const precio = Number( formData.get('precio'))*100  //convertimos € a centimos 
+
+    const articulo = await prisma.articulo.create({
+      data: { nombre, descripcion, precio  },
+    })
+
+    // console.log(articulo);
+    revalidatePath('/articulos')
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+export async function updateArticulo(formData) {
+  const id = Number( formData.get('id') )
+  const nombre = formData.get('nombre')
+  const descripcion = formData.get('descripcion')
+  const precio = Number( formData.get('precio'))*100  //convertimos € a centimos
+
+  try {
+    const articulo = await prisma.articulo.update({
+      where: { id },
+      data:  { nombre, descripcion, precio },
+    })
+
+    // console.log(articulo);
+    revalidatePath('/articulos')
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+
+export async function deleteArticulo(formData) {
+  try {
+    const id = Number( formData.get('id') )
+  
+    const articulo = await prisma.articulo.delete({
+      where: { id },
+    })
+
+    // console.log(articulo);
+    revalidatePath('/articulos')
+  } catch (error) {
+    console.log(error);
+  }
+}
