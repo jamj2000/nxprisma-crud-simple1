b4752986e8032252f180e3a1219a802128fbb393
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-04-18 18:36:15 +0200

    fix prisma again

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-export default const prisma = globalThis.prisma || new PrismaClient()
+export const prisma = globalThis.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prismaº
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
